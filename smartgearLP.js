9d297c490070dea8086a33ec0c7748a72ff2397a
Quentin Vansteene <vansteene.quentin@gmail.com>
2022-11-07 10:46:39 +0100

    add script for section navigation limit scripts to page with 'landingpage' class

--- smartgearLP.js
+++ smartgearLP.js
@@ -1,64 +1,65 @@
 $(function(){
-	var header = $('.headerFW').headerFW('get');
-	header.$el.find('a').add('.goto').on('click',function(e){
-		e.preventDefault();
-		var target = document.querySelector(this.getAttribute('href'));
-		if (target) {
-			if ($(target).innerHeight()<=viewport.height)
-				target.scrollIntoView({behavior: "smooth", block: 'start', inline: "nearest"});
-			else {
-				target.scrollIntoView({behavior: "smooth", block: 'start', inline: "nearest"});
+	if ($('body').hasClass('landingpage')) {
+		var header = $('.headerFW').headerFW('get');
+		header.$el.find('a').add('.goto').on('click',function(e){
+			e.preventDefault();
+			var target = document.querySelector(this.getAttribute('href'));
+			if (target) {
+				if ($(target).innerHeight()<viewport.height)
+					target.scrollIntoView({behavior: "smooth", block: 'start', inline: "nearest"});
+				else {
+					target.scrollIntoView({behavior: "smooth", block: 'start', inline: "nearest"});
+				}
+
 			}
+		});
 
-		}
-	});
+		header.$logo.on('click',function(e){
+			e.preventDefault();
+			document.getElementById('wrapper').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+		});
 
-	header.$logo.on('click',function(e){
-		e.preventDefault();
-		document.getElementById('wrapper').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-	});
+		header.$navPanel.find('a').add(header.$logo).on('click',function(){
+			if (header.$el.hasClass('is-open')) 
+				header.$toggler.trigger('click');
+		})
 
-	header.$navPanel.find('a').add(header.$logo).on('click',function(){
-		if (header.$el.hasClass('is-open')) 
-			header.$toggler.trigger('click');
-	})
+		// section scroll observer
+	    var section_obs = new IntersectionObserver(
+	        (entries, section_obs) => {
+	        	entries.forEach(entry => {
+			        if(entry.isIntersecting) {
+			          // console.log(entry.target);
+			          if (header.$el.find('a[href="#'+entry.target.id+'"]').length) {
+				          	header.$el.find('a').parent('li').removeClass('active');
+				          	header.$el.find('a[href="#'+entry.target.id+'"]').parent('li').addClass('active');
+				          	history.replaceState({}, "", "#"+entry.target.id)
+			          }
+			        }
+			    });
+	        },
+	        { 
+	        	rootMargin: '0px',
+	    		threshold: 0.5 
+	    	}
+	    );
+	    document.querySelectorAll('.mod_article.snap').forEach((i) => {
+	    	if(i) section_obs.observe(i);
+	    });
 
-	// section scroll observer
-    var section_obs = new IntersectionObserver(
-        (entries, section_obs) => {
-        	entries.forEach(entry => {
-		        if(entry.isIntersecting) {
-		          // console.log(entry.target);
-		          if (header.$el.find('a[href="#'+entry.target.id+'"]').length) {
-			          	header.$el.find('a').parent('li').removeClass('active');
-			          	header.$el.find('a[href="#'+entry.target.id+'"]').parent('li').addClass('active');
-			          	history.replaceState({}, "", "#"+entry.target.id)
-		          }
-		        }
-		    });
-        },
-        { 
-        	rootMargin: '0px',
-    		threshold: 0.5 
-    	}
-    );
-    document.querySelectorAll('.mod_article.snap').forEach((i) => {
-    	if(i) section_obs.observe(i);
-    });
+	    $('html').css('scroll-padding',header.$el.outerHeight())
 
-    // $('.ce_rsce_tabs\\+\\+ .sliderFW__nav__item').on('click',function(){})
+		$('.blob').each(function(i,el){
+			// generateBlob(el);
+			if (el.className.includes('animate')){
+				var delay = getComputedStyle(el).transitionDuration.replace('s','') * 1000 * 2;
+				setTimeout(function(){
+					setInterval(function(){generateBlob(el)},delay);
+				},1500*i)
+			}
+		})
+	}
 
-    $('html').css('scroll-padding',header.$el.outerHeight())
-
-	$('.blob').each(function(i,el){
-		// generateBlob(el);
-		if (el.className.includes('animate')){
-			var delay = getComputedStyle(el).transitionDuration.replace('s','') * 1000 * 2;
-			setTimeout(function(){
-				setInterval(function(){generateBlob(el)},delay);
-			},1500*i)
-		}
-	})
 });
 
 
