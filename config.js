6b44ed8b074eb3f5f57e2e2b4ffb7012c769b06f
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-07-10 15:39:08 +0200

    config: set defualt radius to 10px and block-background to white

--- config.js
+++ config.js
@@ -3,9 +3,11 @@ module.exports = {
 		'cream': '#F2F2F2',
 		'blue': '#385D8D'
 	},
+	'radius':'10px',
 	'body': {
 		'font-color': 'colors(grey)',
 		'line-height' 	: '1.35',
+		'block-background' : 'colors(white)'
 	},
 	'btn':{
 		'radius': '50px',
